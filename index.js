f612b481281bc6721626c71479a11f5167ce8b24
Aparna1-ag <aparnagupta.csa@gmail.com>
2025-07-30 10:42:32 +0530

    updated for comparing only upcoming userId for login

--- index.js
+++ index.js
@@ -74,6 +74,40 @@ const faceMatcher = (desOne, desTwo, userName, userId) => {
 };
 
 
+const faceMatcherForLogin = (desOne, desTwo) => {
+  const desOneParsed = JSON.parse(desOne);
+
+  let differenceOfSquares = [];
+
+  for (let a = 0; a < desTwo.length; a++) {
+    let differenesForEachPoint = Number(desOneParsed[a]) - Number(desTwo[a]);
+    differenceOfSquares.push(Math.pow(differenesForEachPoint, 2));
+    // console.log(a, desTwo[a])
+    // console.log(a, desOneParsed[a])
+  }
+
+  // console.log(differenceOfSquares)
+
+  let sumOfDifferences = 0;
+
+  for (let each of differenceOfSquares) {
+    sumOfDifferences += each;
+  }
+
+  // console.log(sumOfDifferences)
+
+  const euclideanDistance = Math.sqrt(sumOfDifferences);
+
+  // console.log(euclideanDistance, username);
+ if (euclideanDistance > 0.6) {
+  return false 
+ } else {
+  return true
+ }
+
+};
+
+
 const findIfNoMatches = (allFacesArrays) => {
   for (let y = 0 ; y < allFacesArrays.length; y++) {
     if  (allFacesArrays[y].distanceIndicator  < 0.6) {
@@ -155,61 +189,114 @@ app.get("/hello", (req, res) => {
 
 
 
-app.post("/compare", async (req, res) => {
-  allComparisons = []
+// app.post("/compare", async (req, res) => {
+//   allComparisons = []
 
-  let { userFace } = req.body;
+//   let { userFace } = req.body;
+//   const currentUserFaceParsed = JSON.parse(userFace);
+//   const [rows] = await pool.query("SELECT * FROM facedata");
+
+
+
+
+
+//   for (let each of rows) {
+//     faceMatcher(each.descriptorArr, currentUserFaceParsed, each.username, each.id);
+//   }
+
+
+//   console.log(allComparisons)
+
+//   let bestMatch = allComparisons[0].distanceIndicator
+//   let bestMatchUserName = allComparisons[0].personName
+//   let bestMatchUserId = allComparisons[0].personId
+
+
+
+//  const matchResult = findIfNoMatches(allComparisons)
+
+
+//  if (matchResult) {
+//   res.json({
+//     success: true,
+//     bestMatchFace: "No matching faces found",
+//     indicator: null
+//   });
+
+
+//  } else {
+//   for (let y = 0 ; y < allComparisons.length; y++) {
+//     console.log(allComparisons[y].distanceIndicator)
+//     if (allComparisons[y].distanceIndicator < bestMatch) {
+//         bestMatch = allComparisons[y].distanceIndicator
+//         bestMatchUserName = allComparisons[y].personName
+//         bestMatchUserId = allComparisons[y].personId
+//         console.log(allComparisons[y].personName, bestMatchUserName)
+//     }
+// }
+
+// res.json({
+//   success: true,
+//   bestMatchFace: bestMatchUserName,
+//   id: bestMatchUserId,
+//   indicator: bestMatch
+// });
+
+
+//  }
+
+  
+
+
+
+
+
+
+// });
+
+
+
+app.post("/loginface", async (req, res) => {
+
+  console.log(req)
+
+  if (!req.body.userId || !req.body.userFace ) {
+    res.status(501).json({
+      error: "Face and User Id are required!"
+    })
+
+    return
+
+  } 
+
+  
+
+  let {  userId, userFace } = req.body;
+
+ 
+
+
   const currentUserFaceParsed = JSON.parse(userFace);
-  const [rows] = await pool.query("SELECT * FROM facedata");
+  const [rows] = await pool.query("SELECT * FROM facedata WHERE ID = ?", [userId]);
 
 
 
 
 
-  for (let each of rows) {
-    faceMatcher(each.descriptorArr, currentUserFaceParsed, each.username, each.id);
-  }
+   const result =  faceMatcherForLogin(rows[0].descriptorArr, currentUserFaceParsed);
 
 
-  console.log(allComparisons)
-
-  let bestMatch = allComparisons[0].distanceIndicator
-  let bestMatchUserName = allComparisons[0].personName
-  let bestMatchUserId = allComparisons[0].personId
-
-
-
- const matchResult = findIfNoMatches(allComparisons)
+   res.status(200).json({
+    success:  true,
+    matchResult : result
+   })
+  
 
 
- if (matchResult) {
-  res.json({
-    success: true,
-    bestMatchFace: "No matching faces found",
-    indicator: null
-  });
+ 
 
 
- } else {
-  for (let y = 0 ; y < allComparisons.length; y++) {
-    console.log(allComparisons[y].distanceIndicator)
-    if (allComparisons[y].distanceIndicator < bestMatch) {
-        bestMatch = allComparisons[y].distanceIndicator
-        bestMatchUserName = allComparisons[y].personName
-        bestMatchUserId = allComparisons[y].personId
-        console.log(allComparisons[y].personName, bestMatchUserName)
-    }
-}
 
-res.json({
-  success: true,
-  bestMatchFace: bestMatchUserName,
-  id: bestMatchUserId,
-  indicator: bestMatch
-});
-
-
- }
 
   
 
@@ -219,6 +306,9 @@ res.json({
 
 
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log("App listening to port " + PORT);
